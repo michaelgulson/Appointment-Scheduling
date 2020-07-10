6d0bc48a9b7c5d6e2226033bab1e5cffc2c30a5c
Michael Gulson <msgulson@gmail.com>
2020-07-09 19:16:39 -0500

    bedefe

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,16 +8,35 @@ import Login from './/Login'
   const Home = () => (
     <div>
     <Header />    
-      <div>
+      <div class="background-image">
         <h2>"Mary S. - "My caregiver from St. Lorraine</h2>
         <h2>helped me recover from my knee surgery through</h2>
         <h2>personalized workouts and meals.</h2>
         <h2>She was a wonderful support to me</h2>
         <h2>and good company during my recovery.</h2>
         <h2>  Thank you St. Lorraine!"</h2>
-      </div>
-      <Login />
-      </div>
+        <br></br>
+        <br></br>
+        <Login />
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+      /</div>
+    </div>
   );
 
   export default Home;
